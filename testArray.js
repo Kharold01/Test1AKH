0c81f18ac8056c2cd50bedfc4429bfb283b7221c
Katie Harold <kharold@sjccs142.saintjoe.edu>
2015-02-18 14:07:33 -0500

    //added an array

---
+++ testArray.js
@@ -0,0 +1,15 @@
+
+//Katie Harold
+//CMP237
+//#3 Declaring an array 
+
+
+// declaring an array of colors
+var array = ["red", 1, "yellow", "green", 5];
+
+// prints the array
+console.log(array);
+// remove last element in the array
+console.log(array.pop());
+// reprint the array
+console.log(array);
